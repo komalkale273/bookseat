6091e8ce2428071f0df4b9f1d8ca425b8d02284a
Komal Kale <kalekomal477@gmail.com>
2025-02-07 00:41:16 +0530

    commit

--- movies/views.py
+++ movies/views.py
@@ -7,84 +7,29 @@ from django.db import IntegrityError, transaction
 from django.http import JsonResponse
 from django.core.mail import send_mail
 from django.conf import settings
+from django.core.paginator import Paginator
 
 
 
 
 def movie_list(request):
-    search_query = request.GET.get('search')
-    if search_query:
-        movies = Movie.objects.filter(name__icontains=search_query)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'movies/movie_list.html', {'movies': movies})
+    search_query = request.GET.get('search', '')
+    movies = Movie.objects.all()
 
+    if search_query:
+        movies = movies.filter(name__icontains=search_query)
+
+    # Pagination: Show 4 movies per page
+    paginator = Paginator(movies, 4)  
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "movies/movie_list.html", {"movies": page_obj})
 
 def theater_list(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
     theaters = Theater.objects.filter(movie=movie)
     return render(request, 'movies/theater_list.html', {'movie': movie, 'theaters': theaters})
-
-
-# @login_required(login_url='/login/')
-# def book_seats(request, theater_id):
-#     theaters = get_object_or_404(Theater, id=theater_id)
-#     seats = Seat.objects.filter(theater=theaters)
-
-#     if request.method == 'POST':
-#         selected_seats = request.POST.getlist('seats') 
-#         error_seats = []
-
-        
-#         if not selected_seats:
-#             return render(request, "movies/seat_selection.html", {
-#                 'theater': theaters, "seats": seats, 'error': "No seats were selected. Please select at least one seat."
-#             })
-
-#         try:
-#             with transaction.atomic():
-#                 for seat_id in selected_seats:
-                    
-#                     seat = get_object_or_404(Seat, id=seat_id, theater=theaters)
-
-#                     if seat.is_booked:  
-#                         continue
-
-                    
-#                     Booking.objects.create(
-#                         user=request.user,
-#                         seat=seat,
-#                         movie=theaters.movie,
-#                         theater=theaters
-#                     )
-
-                    
-#                     seat.is_booked = True
-#                     seat.save()
-
-#         except Exception as e:
-           
-#             print(f"Error during booking: {e}")
-#             return render(request, 'movies/seat_selection.html', {
-#                 'theater': theaters,
-#                 "seats": seats,
-#                 'error': "An error occurred while processing your booking. Please try again."
-#             })
-
-        
-#         if error_seats:
-#             error_message = f"The following seats are already booked: {', '.join(map(str, error_seats))}"
-#             return render(request, 'movies/seat_selection.html', {
-#                 'theater': theaters, "seats": seats, 'error': error_message
-#             })
-
-        
-#         return redirect('profile')
-
-   
-#     return render(request, 'movies/seat_selection.html', {
-#         'theater': theaters, "seats": seats
-#     })
 
 
 @login_required(login_url='/login/')
